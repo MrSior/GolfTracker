078906ee98e0e858116b74872e9e4add4a6d313d
Simon <SemionChubenko@gmail.com>
2021-02-21 23:03:30 +0300

    addListOfPrevGames

---
+++ Golf_Tracker_App/Prev_Game_Results_View.swift
@@ -0,0 +1,30 @@
+//
+//  Prev_Game_Results_View.swift
+//  Golf_Tracker_App
+//
+//  Created by Семён Чубенко on 21.02.2021.
+//  Copyright © 2021 Семён Чубенко. All rights reserved.
+//
+
+import UIKit
+
+class Prev_Game_Results_View: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Golf_Tracker_App/Previous_Games_View.swift
@@ -0,0 +1,30 @@
+//
+//  Previous_Games_View.swift
+//  Golf_Tracker_App
+//
+//  Created by Семён Чубенко on 20.02.2021.
+//  Copyright © 2021 Семён Чубенко. All rights reserved.
+//
+
+import UIKit
+
+class Previous_Games_View: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
